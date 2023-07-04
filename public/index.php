c5aa330a11ca7bd7990896d5170728f5debeb2f9
ericrunnerr <eric.marquesgomesmarques@gmail.com>
2023-07-04 11:08:53 -0300

    Utilizando o foreach para printar os elementos

--- public/index.php
+++ public/index.php
@@ -1,43 +1,12 @@
 <?php
 
-$articles = array([
-    "Heello",
-    "jeelo",
-    "neeko"
-]);
-
-$sorye = "Yasuo";
-$geton = "hasagi";
-
-$ionia = [$sorye, $geton];
-
-$values = [
-    "message" => "Hi everybody my name is markiplier",
-    "count" => 200,
-    "pi" => 3.14,
-    "status" => true,
-    "result" => null
-];
-
-# var_dump($ionia);
-
-# -----------------------------------------------------------------------------------------
-
-# multidimensional array
 $people = [
-    [
-        "name" => "Eric",
-        "age" => 20,
-    ],
-    [
-        "name" => "Miguel",
-        "age" => 10,
-    ],
+    "Eric",
+    "Miguel",
+    "Robson",
+    "Lívia",
 ];
 
-$eric_stuff = $people[0];
-$eric_age = $people[0]["age"];
-
-#var_dump($eric_stuff);
-#var_dump($eric_age);
-var_dump($people[1]["age"]);
+foreach ($people as $people) {
+    echo $people, ", ";
+};
